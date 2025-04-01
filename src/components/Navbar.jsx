433a3157f696dcc8af59e5b7e1d4611e4b92f301
AghilWindfall <aghil@windfallproductions.org>
2025-04-01 14:38:13 +0530

    navbar component created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "../styles/Navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className={styles.headerBtn}>Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
